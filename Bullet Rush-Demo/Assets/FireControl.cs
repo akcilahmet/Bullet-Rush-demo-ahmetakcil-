aba88de962f2045b167c4640b23ddea7c299c5ba
Ahmet Akçıl <83040124+akcilahmet@users.noreply.github.com>
2021-07-10 16:22:40 +0300

    charactercontrol.cs, fireControl.cs, bulletControl.cs set

---
+++ Bullet Rush-Demo/Assets/FireControl.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireControl : MonoBehaviour
+{
+    //fire points
+    public Transform aimpos1;
+    public Transform aimpos;
+    //bullet
+    public GameObject bullet;
+
+    private GameObject currentTarget;
+    public GameObject currentGun;
+
+    public float distance = 500f;
+    private bool isAiming;
+
+    [SerializeField] LayerMask layermask;
+
+    RaycastHit hit;
+
+    void Start()
+    {
+        currentGun.transform.position = aimpos.position;
+    }
+
+
+    private void FixedUpdate()
+    {
+        CheckTarget();
+
+        if (isAiming)
+        {
+            AutoAiming();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void CheckTarget()
+    {
+        if (Physics.Raycast(aimpos.position, aimpos.TransformDirection(new Vector3(0, 0, 1)), out hit, distance, layermask))
+        {
+            Debug.DrawRay(aimpos.position, aimpos.TransformDirection(new Vector3(0, 0, 1)) * hit.distance, Color.red);
+
+            if (hit.transform.gameObject.tag == "Enemy")
+            {
+
+
+                if (!isAiming)
+                {
+                    Fire();
+                    Fire1();
+
+                    currentTarget = hit.transform.gameObject;
+                    isAiming = true;
+
+
+                }
+                else
+                {
+                    currentTarget = null;
+                    isAiming = false;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawRay(aimpos.position, aimpos.TransformDirection(new Vector3(0, 0, 1)) * 1000, Color.white);
+            currentTarget = null;
+            isAiming = false;
+
+        }
+    }
+   
+    private void AutoAiming()
+    {
+
+        currentGun.transform.LookAt(currentTarget.transform);
+
+    }
+    void Fire()
+    {
+        GameObject bulletGO = Instantiate(bullet, aimpos.position, aimpos.rotation);
+    }
+    void Fire1()
+    {
+        GameObject bulletGO = Instantiate(bullet, aimpos1.position, aimpos1.rotation);
+    }
+}
---
+++ Bullet Rush-Demo/Assets/characterMoveControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class characterMoveControl : MonoBehaviour
+{
+    public float Horspeed = 300;
+
+    public float walkingSpeed = 200;
+    float tempSpeed;
+
+    public DynamicJoystick variableJoystick;
+    public Rigidbody rb;
+    Animator anim;
+
+
+    private void Start()
+    {
+        tempSpeed = walkingSpeed;
+        walkingSpeed = 0;
+
+        anim = GetComponent<Animator>();
+        //anim.SetFloat("verticalprm", 0f);
+
+    }
+
+    //public void runStart()
+    //{
+    //    anim.SetTrigger("idleprm");
+
+    //    walkingSpeed = tempSpeed;
+    //    DOTween.To(() => walkingSpeed, x => walkingSpeed = x, tempSpeed, 1);
+    //}
+    public void FixedUpdate()
+    {
+        moveControl();
+    }
+    void moveControl()
+    {
+
+        Vector3 move = new Vector3(variableJoystick.Horizontal * Horspeed * Time.fixedDeltaTime, rb.velocity.y, variableJoystick.Vertical *200* Time.fixedDeltaTime);
+        //float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+        rb.velocity = move;
+
+        anim.SetFloat("horprm", variableJoystick.Horizontal);
+        anim.SetFloat("verprm", variableJoystick.Vertical);
+
+    }
+
+
+
+
+}
